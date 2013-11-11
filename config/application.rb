a0bf81e108db4e541353ae63c27978064a2b625e
ryohei <nikusuki727@gmail.com>
2013-11-11 14:56:47 +0900

    application.rb編集

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Webstdyroom
     config.active_record.whitelist_attributes = true
     config.assets.enabled = true
     config.assets.version = '1.0'
+    config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.assets.precompile += %w( *.css *.js *js.erb )
   end
